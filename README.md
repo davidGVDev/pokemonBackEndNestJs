13a111c8b26f3f6c7beef90d31424033c1acf988
David <david38ing@gmail.com>
2024-04-17 15:03:43 -0600

    Variables de entorno

--- README.md
+++ README.md
@@ -87,7 +87,15 @@ npm i -g @nestjs/cli
 ```
 docker-compose up -d
 ```
-5.- Reconstruir la basede datos con la semilla
+5.- Clonar el archivo __.env.template__ y renombrar la copia a __.env__
+
+6.- Llenar las vcariables de entorno definidas en el ``` .env ```
+
+7.- Ejecutar la aplicacion en dev
+```
+npm run start:dev
+```
+8.- Reconstruir la basede datos con la semilla
 ```
 http://localhost:3000/api/v2/seed
 ```
--- src/pokemon/pokemon.service.ts
+++ src/pokemon/pokemon.service.ts
@@ -10,13 +10,19 @@ import { Model, isValidObjectId } from 'mongoose';
 import { Pokemon } from './entities/pokemon.entity';
 import { InjectModel } from '@nestjs/mongoose';
 import { PaginationDto } from '../common/dto/pagination.dto';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class PokemonService {
+  private defaultLimit: number;
+
   constructor(
+    private readonly configService: ConfigService,
     @InjectModel(Pokemon.name)
     private readonly pokemonModel: Model<Pokemon>,
-  ) {}
+  ) {
+    this.defaultLimit = configService.get('defaulLimit');
+  }
 
   async create(createPokemonDto: CreatePokemonDto) {
     createPokemonDto.name = createPokemonDto.name.toLocaleLowerCase();
@@ -30,7 +36,7 @@ export class PokemonService {
   }
 
   findAll(paginationDto: PaginationDto) {
-    const { limit = 10, offset = 0 } = paginationDto;
+    const { limit = this.defaultLimit, offset = 0 } = paginationDto;
     return this.pokemonModel
       .find()
       .limit(limit)
